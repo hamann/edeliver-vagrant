0fad994bdc06e7cb9331cb2813bd711f6e1e9f54
Holger Amann <holger@sauspiel.de>
2015-09-16 14:05:54 -0700

    Add missing configuration settings to ~./secrets/ed/prod.config
    LINK_SYS_CONFIG will override all settings generated from config/*.config

--- bootstrap/secrets.sh
+++ bootstrap/secrets.sh
@@ -6,6 +6,22 @@ cat << EOF | tee ~/.secrets/ed/prod.config
 [
   {ed,
     [
+      {'Elixir.Ed.Endpoint',
+        [
+          {http, [{port, 4000}]},
+          {url, [{host, <<"example.com">>}]},
+          {cache_static_manifest, <<"priv/static/manifest.json">>},
+          {server, true},
+          {secret_key_base, <<"SXyyEevkSQt4kUdYWV2nODXWrMKqwDn4fFl5wWM3vVA/U3wv/XjaZa8FqBjODRlW">>},
+          {pubsub, [{name, 'Elixir.Ed.PubSub'},{adapter, 'Elixir.Phoenix.PubSub.PG2'}]},
+          {root, <<"/home/vagrant/deliver/ed">>}
+        ]
+      },
+      {logger,
+        [
+          {level, info}
+        ]
+      },
       {'Elixir.Ed.Repo',
         [
           {adapter,'Ecto.Adapters.Postgres'},
